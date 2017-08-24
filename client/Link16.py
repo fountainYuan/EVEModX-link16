d970344f63e6c066f7a347da48e5e5ea6c07b0a7
韩于惟 <hanyuwei70@qq.com>
2017-08-24 17:00:39 +0800

    初步弄出协议

---
+++ client/Link16.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# EVEModX 战场数据链
+# Python 2
+
+class Link16Conn(object):
+
+    def connect(self,addr):
---
+++ server.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# Link16 服务器端
+# Python 3
+
+# 3rd party lib
+import tornado.tcpserver
+
+
+def main():
+    pass
+
+
+if __name__ == "main":
+    main()
+
+
